ee53ebcd709e88c998882f69e9ff81bf1a94a672
Hasan Kara <hasan.selman.kara@gmail.com>
2020-02-05 20:48:32 +0100

    Refactor

--- src/main/java/li/selman/jpbe/dsl/position/ConstantPosition.java
+++ src/main/java/li/selman/jpbe/dsl/position/ConstantPosition.java
@@ -5,15 +5,21 @@ package li.selman.jpbe.dsl.position;
  */
 public class ConstantPosition implements Position {
 
+    private static final int LAST_POSITION_INDEX = Integer.MIN_VALUE;
+
     private final int constant;
 
     public ConstantPosition(int constant) {
         this.constant = constant;
     }
 
+    public static ConstantPosition lastPosition() {
+        return new ConstantPosition(LAST_POSITION_INDEX);
+    }
+
     @Override
     public int evalToPosition(String s) {
-        if (constant == Integer.MIN_VALUE) {
+        if (constant == LAST_POSITION_INDEX) {
             return s.length();
         }
 
