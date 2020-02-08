2c5986a80955d051304bae72f562a23dc174bd97
Hasan Kara <hasan.selman.kara@gmail.com>
2020-02-08 16:26:09 +0100

    Add mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // Use JUnit Jupiter API for testing.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
 	testImplementation("org.assertj:assertj-core:3.15.0")
+	testImplementation("org.mockito:mockito-core:3.2.4")
 
     // Use JUnit Jupiter Engine for testing.
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
