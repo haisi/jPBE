2776097d7bf556cadd0f07c83439eea1c7b2b163
Hasan Kara <hasan.selman.kara@gmail.com>
2021-02-26 21:27:33 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,14 @@ plugins {
 
 dependencies {
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:28.1-jre")
+    implementation("com.google.guava:guava:30.1-jre")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testImplementation("org.assertj:assertj-core:3.15.0")
-    testImplementation("org.mockito:mockito-core:3.2.4")
-
-    // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    val junitVersion = "5.7.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.mockito:mockito-core:3.8.0")
 }
 
 val test by tasks.getting(Test::class) {
