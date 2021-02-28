d7917dd2cd586d0d4f044cb903456caeb59f5528
Hasan Kara <hasan.selman.kara@gmail.com>
2021-02-28 13:30:03 +0100

    Add JavaDoc to DslElement

--- src/main/java/li/selman/jpbe/dsl/DslElement.java
+++ src/main/java/li/selman/jpbe/dsl/DslElement.java
@@ -8,6 +8,20 @@ package li.selman.jpbe.dsl;
  */
 public interface DslElement {
 
+    /**
+     * The weight can be used to find an optimal program in a set of valid programs.
+     * "Optimal" is subjective and can go into different directions.
+     * Some examples include:
+     * <li>
+     *     <ul>Weight by how expensive a DSL element is to execute.</ul>
+     *     <ul>
+     *         Weight by how likely a DSL element is to be part of a generic full program.
+     *         A ConstantPosition generally is less generic.
+     *         Thus, the user has to specify their intent more often to finish their wrangling task.
+     *     </ul>
+     * </li>
+     * @return the weight of the DSL element
+     */
     int getDslWeight();
 
 }
