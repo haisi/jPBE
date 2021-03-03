f4f9f0e4fa354b980872ba9430e0243ad3141ac3
Hasan Kara <hasan.selman.kara@gmail.com>
2021-03-03 21:54:09 +0100

    Add more tests for positions builder

--- src/test/java/li/selman/jpbe/dsl/position/PositionsBuilderTest.java
+++ src/test/java/li/selman/jpbe/dsl/position/PositionsBuilderTest.java
@@ -15,45 +15,38 @@ import java.util.Set;
 import li.selman.jpbe.dsl.token.TokenSequence;
 import li.selman.jpbe.dsl.token.TokenSequenceBuilder;
 import li.selman.jpbe.dsl.token.Tokens;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("checkstyle:AbbreviationAsWordInName")
 class PositionsBuilderTest {
 
-    @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
+    Tokens supportedTokens = new Tokens(List.of(START, END, ALPHA, SPACE));
+    int maxTokenSeqLength = 999;
+    PositionsBuilder positionBuilder = PositionsBuilder.of(
+        new ConstantPositionBuilder(),
+        new DynamicPositionBuilder(new TokenSequenceBuilder(maxTokenSeqLength, supportedTokens))
+    );
+
+    // Token-Sequences S: Space, A: Alphanumeric, s: Start, e: End
+    private final TokenSequence S = TokenSequence.of(SPACE);
+    private final TokenSequence A = TokenSequence.of(ALPHA);
+    private final TokenSequence e = TokenSequence.of(END);
+    private final TokenSequence Ae = TokenSequence.of(ALPHA, END);
+    private final TokenSequence AS = TokenSequence.of(ALPHA, SPACE);
+    private final TokenSequence s = TokenSequence.of(START);
+    private final TokenSequence sA = TokenSequence.of(START, ALPHA);
+    private final TokenSequence sAS = TokenSequence.of(START, ALPHA, SPACE);
+    private final TokenSequence sASA = TokenSequence.of(START, ALPHA, SPACE, ALPHA);
+    private final TokenSequence sASAe = TokenSequence.of(START, ALPHA, SPACE, ALPHA, END);
+    private final TokenSequence ASAe = TokenSequence.of(ALPHA, SPACE, ALPHA, END);
+    private final TokenSequence SAe = TokenSequence.of(SPACE, ALPHA, END);
+
     @Test
-    @Disabled
-    void fooTest() {
+    void simple1Test() {
         // given
         String input = "AB CDE";
         int expectedPosition = 3;
 
-        // Token-Sequences S: Space, A: Alphanumeric, s: Start, e: End
-        TokenSequence S = TokenSequence.of(SPACE);
-        TokenSequence A = TokenSequence.of(ALPHA);
-        TokenSequence e = TokenSequence.of(END);
-        TokenSequence Ae = TokenSequence.of(ALPHA, END);
-        TokenSequence AS = TokenSequence.of(ALPHA, SPACE);
-        TokenSequence s = TokenSequence.of(START);
-        TokenSequence sA = TokenSequence.of(START, ALPHA);
-        TokenSequence sAS = TokenSequence.of(START, ALPHA, SPACE);
-        TokenSequence sASA = TokenSequence.of(START, ALPHA, SPACE, ALPHA);
-        TokenSequence sASAe = TokenSequence.of(START, ALPHA, SPACE, ALPHA, END);
-        TokenSequence ASAe = TokenSequence.of(ALPHA, SPACE, ALPHA, END);
-        TokenSequence SAe = TokenSequence.of(SPACE, ALPHA, END);
-
-        Tokens supportedTokens = new Tokens(List.of(START, END, ALPHA, SPACE));
-        int maxTokenSeqLength = 999;
-        var positionBuilder = PositionsBuilder.of(
-            new ConstantPositionBuilder(),
-            new DynamicPositionBuilder(new TokenSequenceBuilder(maxTokenSeqLength, supportedTokens)) // Buggy
-        );
-
-        // when
-        Set<Position> actual = positionBuilder.computePositions(input, expectedPosition);
-
-        // then
-        // s: "AB CDE", k: 3
         Set<Position> expected = Set.of(
             new ConstantPosition(3),
             new ConstantPosition(-3),
@@ -71,13 +64,79 @@ class PositionsBuilderTest {
             new DynamicPosition(S, Ae, -1)
         );
 
-        // TODO(#bug): PositionBuilder generates positions which do not eval to any position on the same input!
-        actual.forEach((pos) -> {
-            int actualPosition = pos.evalToPosition(input);
-            assertThat(actualPosition).isEqualTo(expectedPosition);
-        });
-//        assertThat(actual).containsAll(expected);
-//        assertThat(actual).containsExactlyElementsOf(expected);
+        executeAndAssert(input, expectedPosition, expected);
+    }
+
+    @Test
+    void simple2Test() {
+        // given
+        String input = "AB CDE";
+        int expectedPosition = 2;
+
+        Set<Position> expected = Set.of(
+            new ConstantPosition(2),
+            new ConstantPosition(-4),
+            new DynamicPosition(sA, SAe, 1),
+            new DynamicPosition(sA, SAe, -1),
+            new DynamicPosition(A, SAe, 1),
+            new DynamicPosition(A, SAe, -1)
+        );
+
+        executeAndAssert(input, expectedPosition, expected);
+    }
+
+    @Test
+    void lastPositionTest() {
+        // given
+        String input = "AB CDE";
+        int expectedPosition = 6;
+
+        Set<Position> expected = Set.of(
+            new ConstantPosition(6),
+            ConstantPosition.lastPosition(),
+            new DynamicPosition(sASAe, e, 1),
+            new DynamicPosition(sASAe, e, -1),
+            new DynamicPosition(ASAe, e, 1),
+            new DynamicPosition(ASAe, e, -1),
+            new DynamicPosition(SAe, e, 1),
+            new DynamicPosition(SAe, e, -1),
+            new DynamicPosition(Ae, e, 1),
+            new DynamicPosition(Ae, e, -1)
+        );
+
+        executeAndAssert(input, expectedPosition, expected);
+    }
+
+    @Test
+    void firstPositionTest() {
+        // given
+        String input = "AB CDE";
+        int expectedPosition = 0;
+
+        Set<Position> expected = Set.of(
+            new ConstantPosition(0),
+            new ConstantPosition(-6),
+            new DynamicPosition(s, sASAe, 1),
+            new DynamicPosition(s, sASAe, -1),
+            new DynamicPosition(s, sASA, 1),
+            new DynamicPosition(s, sASA, -1),
+            new DynamicPosition(s, sAS, 1),
+            new DynamicPosition(s, sAS, -1),
+            new DynamicPosition(s, sA, 1),
+            new DynamicPosition(s, sA, -1)
+        );
+
+        executeAndAssert(input, expectedPosition, expected);
+    }
+
+    private void executeAndAssert(String input, int k, Set<Position> expected) {
+        // when
+        Set<Position> actual = positionBuilder.computePositions(input, k);
+
+        // then
+        assertThat(actual).allMatch(position -> position.evalToPosition(input) == k);
+        assertThat(actual).hasSizeGreaterThan(2);
+        assertThat(actual).containsAll(expected);
     }
 
 }
