8c1d4de89224da1ee6ee759dd098bce1651cc7a2
Hasan Kara <hasan.selman.kara@gmail.com>
2021-03-05 22:13:26 +0100

    Add Javadoc to Edge class

--- src/main/java/li/selman/jpbe/datastructure/Edge.java
+++ src/main/java/li/selman/jpbe/datastructure/Edge.java
@@ -7,6 +7,19 @@ import java.util.Set;
 import li.selman.jpbe.dsl.Expression;
 
 /**
+ * An edge represents a set of programs which evaluate to a substring of the intended output string.
+ * An edge is part of a directed acyclic graph (DAG).
+ * <p>
+ * For example: Given input {@code i_1} and output {@code o_1}, we generate a graph with
+ * {@code n * (n - 1) / 2} edges where {@code n = o_1.length},
+ * because we generate an edge for each possible substring made out of {@code o_1}.
+ * {@link Edge#from} represents the start index of such substring and {@link Edge#to} the end index.
+ * While {@link Edge#expressions} contains all valid expressions in our DSL for which following constraint holds true.
+ * <p>
+ * For each expression {@code exp} in Expression | {@code exp.apply(i_1) == o_1.substring(from, to)}
+ * <p>
+ * For example: Given i_1 = "Joe Bob 1990.12.3" and output o_1 = "Bob 1990", we might have the following edge:
+ * {@code Edge(0, 3, { CStr("Bob"), SubStr(CPos(4), CPos(7), ... })} where all expression on i_1 would eval to "Bob".
  * @author Hasan Selman Kara
  */
 // TODO(#api): turn into record
@@ -16,6 +29,11 @@ public class Edge {
     public final int to;
     private final Set<Expression> expressions;
 
+    /**
+     * @param from start index on a substring of the user provided output string
+     * @param to end index on a substring of the user provided output string
+     * @param expressions all expressions which on input_1 eval to provided_output_1.substring(from, to)
+     */
     public Edge(int from, int to, Set<Expression> expressions) {
         if (from >= to) throw new IllegalArgumentException(String.format("Edge cannot go from '%d' to '%d'", from, to));
         if (expressions.isEmpty()) throw new IllegalArgumentException("Expressions cannot be empty");
