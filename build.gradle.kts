cbb49c4267140c5cb84fc33dea0e7c0651547af5
Hasan Kara <hasan.selman.kara@gmail.com>
2021-12-07 18:03:28 +0100

    Update to Gradle 7.3 and latest palantir baseline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import net.ltgt.gradle.errorprone.errorprone
+import net.ltgt.gradle.errorprone.CheckSeverity
 
 buildscript {
     repositories {
@@ -18,8 +19,8 @@ plugins {
     // Part of palantir baseline sanity checks.
     // Processor for Google's "Error Prone" tool.
     id("org.inferred.processors") version "3.6.0"
-    id("com.palantir.baseline") version "4.25.0"
-    id("org.asciidoctor.jvm.convert") version "3.1.0"
+    id("com.palantir.baseline") version "4.47.0"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
 
 java {
@@ -30,32 +31,39 @@ java {
 asciidoctorj {
     modules {
         diagram.use()
-        diagram.setVersion("2.0.5")
+        diagram.setVersion("2.2.1")
     }
 }
 
 dependencies {
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:30.1-jre")
-    val log4jVersion = "2.14.0"
+    val log4jVersion = "2.14.1"
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
 
     // Use JUnit Jupiter API for testing.
-    val junitVersion = "5.7.1"
+    val junitVersion = "5.8.2"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    testImplementation("org.assertj:assertj-core:3.19.0")
-    testImplementation("org.mockito:mockito-core:3.8.0")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.mockito:mockito-core:4.1.0")
 }
 
 tasks.withType<JavaCompile>().configureEach {
     options.errorprone.disable("MissingSummary", "EqualsGetClass", "OptionalOrElseMethodInvocation",
-        "PreferSafeLoggableExceptions", "PreferSafeLoggingPreconditions",
-        "StrictUnusedVariable" // Re-enable in the future
+            "PreferSafeLoggableExceptions", "PreferSafeLoggingPreconditions",
+            "StrictUnusedVariable" // Re-enable in the future
     )
+
+    options.errorprone {
+        check("PreferSafeLogger", CheckSeverity.OFF)
+        check("PreferSafeLoggingPreconditions", CheckSeverity.OFF)
+        check("PreferSafeLoggableExceptions", CheckSeverity.OFF)
+        check("Slf4jLogsafeArgs", CheckSeverity.OFF)
+    }
 }
 
 tasks.named<JavaCompile>("compileTestJava") {
